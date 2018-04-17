4232b26694571c2452650283e2460e263a93483d
Diogo Ide <diogoide@gmail.com>
2018-04-17 00:18:06 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,6 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
        BRANDS.map do |repetition|
+         binding.pry
         if repetition != brand
            BRANDS << brand
         else
