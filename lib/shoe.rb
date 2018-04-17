6dc73167acd49ee87a2cd7bdfeb0d6c91872f266
Diogo Ide <diogoide@gmail.com>
2018-04-17 00:11:53 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand 
+    BRANDS << brand if @brand != brand 
     #   BRANDS.each do |brand|
     #     if brand == @brand
     #       nil
